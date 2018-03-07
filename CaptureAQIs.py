f57ec87b8cdb61d031a3002686cc18c4450758d5
Raymond Zhang <mb65411@umac.mo>
2018-03-07 23:41:36 +0800

    add the function to re-download the data for the city not updated in previous update and fix some bugs

--- CaptureAQIs.py
+++ CaptureAQIs.py
@@ -13,16 +13,26 @@ import requests
 import pandas as pd
 import pickle
 import datetime
+import traceback
+from sklearn.datasets import base 
+import time
+import random
 
-def previous_time():
+def check_pre_update(CityList):
       try:
-            _,_,time_point = pd.read_pickle('AQIsData/thisUpdate.pickle')
+#            _,_,time_point,preErrorCities = pd.read_pickle('AQIsData/thisUpdate.pickle')
+            update = pd.read_pickle('AQIsData/thisUpdate.pickle')
+            time_point = update.time
+            preErrorCities = update.notUpdatedCity
+            
       except:
             time_point = ''
-      return time_point
+            preErrorCities = []
+      return time_point,preErrorCities
 
+            
 def download_data(CityList):
-      previous_time_point = previous_time()
+      previous_time_point,preErrorCities = check_pre_update(CityList)
       Full_stations = City_only = pd.DataFrame()
       url = 'http://www.pm25.in/api/querys/aqi_details.json'
       #url = 'http://www.pm25.in/api/querys/all_cities.json'
@@ -30,58 +40,93 @@ def download_data(CityList):
       header = {'content-type': 'application/json; charset=utf-8',
                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0',
                   'Connection': 'close'}
-      ErrorCities = [] # 由于网络等原因未能下载到数据的城市，记录之并稍后再次访问
-      for i in range(len(CityList)):
-            city = CityList[i]
-#            print(' loading: {}'.format(city))
-            param  = {'city':city,'token':token}
-            try:
-                r = requests.get(url, params = param,headers=header,timeout=10)
-            except Exception as e:
-                ErrorCities.append(city) 
-                log('[Request Error]  City: [{}] is unable to download.  --> Error: {}'.format(city,e))
-                continue
-            code = r.status_code
-            #判断是否通信成功
-            if code == 200:
-#                  log('GET request OK： {}'.format(city))
-                  content = r.json()
-                  if isinstance(content, dict):
-                        log('[Failed]   token of API is out of use: {}'.format(city))
-                        return
-                  elif isinstance(content, list):
-                        # 获取此次更新的时间： 选择最后一条（for city），以免某些废站点的数据误导
-                        time_point = content[-1]['time_point'] 
-                        # 判断是否与上次获取数据的time_point,相同则取消此次更新
-                        if time_point == previous_time_point:
-                              log('[Canceled] Same as the previous')
-                              return
-                        else:
-                              city_data = pd.DataFrame(content).fillna('')
-                              Full_stations = Full_stations.append(city_data, ignore_index=True)
-                              City_only = City_only.append(city_data.iloc[-1], ignore_index=True)
-            else:
-                  ErrorCities.append(city) 
-                  log('[Request Error]    GET request error {}: {}'.format(code,city))
-            
-
-      if len(ErrorCities)==0:
-            infor = '[Success]  Updated all cities!     TimePoint: {}'.format(str(time_point))
-      elif 0<len(ErrorCities)<len(CityList):
-            infor = '[Success]  Updated some of cities! TimePoint: {}   --> Not updated: {}'.format(str(time_point),ErrorCities)
-      elif len(ErrorCities)==len(CityList):
-            log('[Failed]   No cities are updated! ')
-            return
-      return [infor,[Full_stations,City_only]]
+      NEW_TIME_POINT = True
+      ErrorCities = [] # 由于网络等原因未能下载到数据的城市，记录之并稍后再次更新
+      update_cities_for_pre = [] # 此次更新的上次未更新之城市
+      ErrorCities_for_pre = []   # # 此次未能更新的上次未更新之城市
       
+      for city in CityList:
+            # 【判断时间戳及未更新城市】 如果是上次的时间戳且该城市已更新过，则跳过
+            if NEW_TIME_POINT == False and city not in preErrorCities:
+                  continue
+            else:
+                  param  = {'city':city,'token':token}
+                  try:
+                      r = requests.get(url, params = param,headers=header,timeout=10)
+                  except Exception as e:
+                        if NEW_TIME_POINT == True:
+                              ErrorCities.append(city)
+                        else:
+                              ErrorCities_for_pre.append(city)
+                        log('[Request Error]  City: [{}] is unable to download.  --> Error: {}'.format(city,e))
+                        continue
+                  code = r.status_code
+                  #判断是否通信成功
+                  if code == 200:
+      #                  log('GET request OK： {}'.format(city))
+                        content = r.json()
+                        if isinstance(content, dict):
+                              log('[Failed]   token of API is out of use: {}'.format(city))
+                              return
+                        elif isinstance(content, list):
+                              # 获取此次更新的时间： 选择最后一条（for city），以免某些废站点的数据误导
+                              time_point = content[-1]['time_point']
+                              # 判断是否与上次获取数据的时间点,不同则执行此次更新
+                              if time_point != previous_time_point:
+                                    NEW_TIME_POINT = True
+                                    city_data = pd.DataFrame(content).fillna('')
+                                    Full_stations = Full_stations.append(city_data, ignore_index=True)
+                                    City_only = City_only.append(city_data.iloc[-1], ignore_index=True)
+                              # 时间点相同
+                              else:
+                                    NEW_TIME_POINT = False
+                                    if len(preErrorCities) == 0:
+                                          log('[Canceled] Same as the previous')
+                                          return
+                                    else:
+                                          city_data = pd.DataFrame(content).fillna('')
+                                          Full_stations = Full_stations.append(city_data, ignore_index=True)
+                                          City_only = City_only.append(city_data.iloc[-1], ignore_index=True)
+                                          update_cities_for_pre.append(city)
+                                    
+                  else:
+                        if NEW_TIME_POINT == True:
+                              ErrorCities.append(city)
+                        else:
+                              ErrorCities_for_pre.append(city)
+                        log('[Request Error]    GET request error {}: {}'.format(code,city))
+                  
+      # 纯更新
+      if NEW_TIME_POINT == True:
+            if len(ErrorCities)==0:
+                  infor = '[Success]  Updated all cities!       TimePoint: {}'.format(str(time_point))
+            elif 0<len(ErrorCities)<len(CityList):
+                  infor = '[Success]  Updated some of cities!   TimePoint: {}   --> Not updated: {}'.format(str(time_point),ErrorCities)
+            elif len(ErrorCities)==len(CityList):
+                  log('[Failed]   No cities are updated! ')
+                  return
+            return [infor,[Full_stations,City_only,ErrorCities]]
+      
+      # 更新上次未更新的城市
+      elif len(preErrorCities) != 0:
+            if len(update_cities_for_pre) == 0:
+                  log('[Failed]   No cities are updated for previous (TimePoint: {}) not-updated city(s)!'.format(previous_time_point))
+                  return
+            elif len(update_cities_for_pre) < len(preErrorCities):
+                  infor = '[Success]  Updated: {}    Not updated: {}   TimePoint: {}'.format(update_cities_for_pre,ErrorCities_for_pre,str(time_point))
+            elif len(update_cities_for_pre) == len(preErrorCities):
+                  infor = '[Success]  Updated all previous not-updated cities!     TimePoint: {}   Updated: {}'.format(str(time_point),update_cities_for_pre)
+            return [infor,[Full_stations,City_only,ErrorCities_for_pre]]
                  
+      
+      
 def update_to_pickle(data):
-      Full_stations,City_only = data
+      Full_stations,City_only,ErrorCities = data
       # 改变列顺序，使之更易读
       columns = ['time_point','area','position_name','station_code','aqi','quality',
-           'primary_pollutant','pm2_5', 'pm2_5_24h','pm10', 'pm10_24h',
-           'co', 'co_24h', 'no2', 'no2_24h', 'o3', 'o3_24h', 
-           'o3_8h', 'o3_8h_24h','so2', 'so2_24h']
+                 'primary_pollutant','pm2_5', 'pm2_5_24h','pm10', 'pm10_24h',
+                 'co', 'co_24h', 'no2', 'no2_24h', 'o3', 'o3_24h', 
+                 'o3_8h', 'o3_8h_24h','so2', 'so2_24h']
       Full_stations = Full_stations.reindex(columns=columns)
       City_only = City_only.reindex(columns=columns)
       City_only.pop('position_name')
@@ -91,7 +136,11 @@ def update_to_pickle(data):
       
       # 保存此次update的数据
       with open('AQIsData/thisUpdate.pickle', 'wb') as file:
-            pickle.dump([Full_stations, City_only, time_point], file)
+            date = base.Bunch(full = Full_stations, 
+                              city = City_only, 
+                              time = time_point,
+                              notUpdatedCity = ErrorCities)
+            pickle.dump(date, file)
             
       # 将更新并入历史数据
       month = time_point[:7]
@@ -100,12 +149,20 @@ def update_to_pickle(data):
       import os
       if os.path.exists(filepath):
             try:
-                  Full_his, City_his, time_his = pd.read_pickle(filepath)
+#                  Full_his, City_his, time_his = pd.read_pickle(filepath)
+                  his = pd.read_pickle(filepath)
+                  Full_his = his.full  
+                  City_his = his.city
+                  time_his = his.time
             except Exception as e:
                   # 如无法获取该月份的历史数据，为了避免覆写历史数据的误操作，将本次更新的数据另建一pickle，以待后续手动合并
                   filename = 'not-merged-Data-{}.pickle'.format(time_point)
                   with open('AQIsData/'+filename, 'wb') as file:
-                        pickle.dump([Full_stations, City_only, time_point], file)
+                        data = base.Bunch(full = Full_stations, 
+                                                     city = City_only, 
+                                                     time = time_point,
+                                                     notUpdatedCity = ErrorCities)
+                        pickle.dump(data, file)
                   log('[Error]  Fail to load [{}] and unable to merge into his data. \
                                      Create an extra file:{}.  ({})'.format(his_filename,filename,e))
                   return 
@@ -120,7 +177,10 @@ def update_to_pickle(data):
       City_his = pd.concat([City_only, City_his], axis=0, join='outer', ignore_index=True)
       time_his = pd.Series(time_point).append(time_his,ignore_index=True)
       with open(filepath, 'wb') as file:
-            pickle.dump([Full_his, City_his, time_his], file)
+            his = base.Bunch(full = Full_his, 
+                             city = City_his, 
+                             time = time_his)
+            pickle.dump(his, file)
 
 def log(infor):
       #filepath = r'AQIsData/'+filename
@@ -140,8 +200,10 @@ def log(infor):
 
       
 def main():
-      CityList = {0:'guangzhou',1:'zhaoqing',2:'foshan',3:'huizhou',4:'dongguan',
-                  5:'zhongshan',6:'shenzhen',7:'jiangmen',8:'zhuhai'}
+      CityList = ['guangzhou','zhaoqing','foshan','huizhou','dongguan',
+                  'zhongshan','shenzhen','jiangmen','zhuhai']
+      
+      time.sleep(random.uniform(1, 59))
       
       data = download_data(CityList)
       
@@ -152,7 +214,10 @@ def main():
             log(infor)
             
       
-if __name__ == '__main__':
-      main()
+if __name__ == '__main__':      
+      try:
+            main()
+      except Exception:
+            log('[Error] \n{}'.format(traceback.format_exc()))
       
       
